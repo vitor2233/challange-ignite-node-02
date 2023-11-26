0f1d4db230e437824b70b0458475a19233584dea
vitor2233 <vitorguilherme2233@gmail.com>
2023-11-26 09:49:42 -0300

    CRUD meals

--- src/routes/meals.ts
+++ src/routes/meals.ts
@@ -5,10 +5,8 @@ import { knex } from "../database";
 
 export async function mealsRoute(app: FastifyInstance) {
     app.get('/', async (request, reply) => {
-        let sessionId = request.cookies.sessionId
-        console.log(sessionId);
-
-        return reply.status(200).send()
+        const meals = await knex('meals').select('*')
+        return reply.status(200).send(meals)
     })
     app.post('/', {
         preHandler: [checkSessionIdExists]
@@ -33,5 +31,41 @@ export async function mealsRoute(app: FastifyInstance) {
         })
 
         return reply.status(201).send()
+    })
+    app.put('/:id', {
+        preHandler: [checkSessionIdExists]
+    }, async (request, reply) => {
+        const createMealBodySchema = z.object({
+            name: z.string(),
+            description: z.string(),
+            dateTime: z.string(),
+            isInDiet: z.boolean()
+        })
+        const getTransactionParamsSchema = z.object({
+            id: z.string().uuid(),
+        })
+        const { name, description, dateTime, isInDiet } = createMealBodySchema.parse(request.body)
+        const { id } = getTransactionParamsSchema.parse(request.params);
+
+        await knex('meals').update({
+            name,
+            description,
+            date_time: dateTime,
+            is_in_diet: isInDiet
+        }).where('id', id)
+
+        return reply.status(200).send()
+    })
+    app.delete('/:id', {
+        preHandler: [checkSessionIdExists]
+    }, async (request, reply) => {
+        const getTransactionParamsSchema = z.object({
+            id: z.string().uuid(),
+        })
+        const { id } = getTransactionParamsSchema.parse(request.params);
+
+        await knex('meals').delete().where('id', id)
+
+        return reply.status(200).send()
     })
 }
